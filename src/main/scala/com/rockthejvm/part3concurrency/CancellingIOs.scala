d44d6f85b1720c9515a8f29de4bab38a80a4cdcb
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2021-07-29 18:35:57 +0300

    cancelling ios

---
+++ src/main/scala/com/rockthejvm/part3concurrency/CancellingIOs.scala
@@ -0,0 +1,78 @@
+package com.rockthejvm.part3concurrency
+
+import cats.effect.{IO, IOApp}
+
+import scala.concurrent.duration.*
+
+object CancellingIOs extends IOApp.Simple {
+
+  import com.rockthejvm.utils.*
+
+  /*
+    Cancelling IOs
+    - fib.cancel
+    - IO.race & other APIs
+    - manual cancellation
+   */
+  val chainOfIOs: IO[Int] = IO("waiting").debug >> IO.canceled >> IO(42).debug
+
+  // uncancelable
+  // example: online store, payment processor
+  // payment process must NOT be canceled
+  val specialPaymentSystem = (
+    IO("Payment running, don't cancel me...").debug >>
+    IO.sleep(1.second) >>
+    IO("Payment completed.").debug
+  ).onCancel(IO("MEGA CANCEL OF DOOM!").debug.void)
+
+  val cancellationOfDoom = for {
+    fib <- specialPaymentSystem.start
+    _ <- IO.sleep(500.millis) >> fib.cancel
+    _ <- fib.join
+  } yield ()
+
+  val atomicPayment = IO.uncancelable(_ => specialPaymentSystem) // "masking"
+  val atomicPayment_v2 = specialPaymentSystem.uncancelable // same
+
+  val noCancellationOfDoom = for {
+    fib <- atomicPayment.start
+    _ <- IO.sleep(500.millis) >> IO("attempting cancellation...").debug >> fib.cancel
+    _ <- fib.join
+  } yield ()
+
+  /*
+    The uncancelable API is more complex and more general.
+    It takes a function from Poll[IO] to IO. In the example above, we aren't using that Poll instance.
+    The Poll object can be used to mark sections within the returned effect which CAN BE CANCELED.
+   */
+
+  /*
+    Example: authentication service. Has two parts:
+    - input password, can be cancelled, because otherwise we might block indefinitely on user input
+    - verify password, CANNOT be cancelled once it's started
+   */
+  val inputPassword = IO("Input password:").debug >> IO("(typing password)").debug >> IO.sleep(2.seconds) >> IO("RockTheJVM1!")
+  val verifyPassword = (pw: String) => IO("verifying...").debug >> IO.sleep(2.seconds) >> IO(pw == "RockTheJVM1!")
+
+  val authFlow: IO[Unit] = IO.uncancelable { poll =>
+    for {
+      pw <- poll(inputPassword).onCancel(IO("Authentication timed out. Try again later.").debug.void) // this is cancelable
+      verified <- verifyPassword(pw) // this is NOT cancelable
+      _ <- if (verified) IO("Authentication successful.").debug // this is NOT cancelable
+           else IO("Authentication failed.").debug
+    } yield ()
+  }
+
+  val authProgram = for {
+    authFib <- authFlow.start
+    _ <- IO.sleep(3.seconds) >> IO("Authentication timeout, attempting cancel...").debug >> authFib.cancel
+    _ <- authFib.join
+  } yield ()
+
+  /*
+    Uncancelable calls are MASKS which suppress cancellation.
+    Poll calls are "gaps opened" in the uncancelable region.
+   */
+
+  override def run = authProgram
+}
