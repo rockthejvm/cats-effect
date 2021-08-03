402dd4ae458df5465beccd6c6563bb73512f9d05
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2021-08-03 15:46:12 +0300

    termporal

--- src/main/scala/com/rockthejvm/part4coordination/Mutex.scala
+++ src/main/scala/com/rockthejvm/part4coordination/Mutex.scala
@@ -16,53 +16,6 @@ import cats.syntax.flatMap._
 import cats.syntax.functor._
 import cats.effect.syntax.monadCancel._
 
-// generic mutex after the polymorphic concurrent exercise
-abstract class MutexV2[F[_]] {
-  def acquire: F[Unit]
-  def release: F[Unit]
-}
-
-object MutexV2 {
-  type Signal[F[_]] = Deferred[F, Unit]
-  case class State[F[_]](locked: Boolean, waiting: Queue[Signal[F]])
-
-  def unlocked[F[_]] = State[F](locked = false, Queue())
-  def createSignal[F[_]](using concurrent: Concurrent[F]): F[Signal[F]] = concurrent.deferred[Unit]
-
-  def create[F[_]](using concurrent: Concurrent[F]): F[MutexV2[F]] =
-    concurrent.ref(unlocked).map(initialState => createMutexWithCancellation(initialState))
-
-  def createMutexWithCancellation[F[_]](state: Ref[F, State[F]])(using concurrent: Concurrent[F]): MutexV2[F] =
-    new MutexV2[F] {
-      override def acquire = concurrent.uncancelable { poll =>
-        createSignal.flatMap { signal =>
-
-          val cleanup = state.modify {
-            case State(locked, queue) =>
-              val newQueue = queue.filterNot(_ eq signal)
-              State(locked, newQueue) -> release
-          }.flatten
-
-          state.modify {
-            case State(false, _) => State[F](locked = true, Queue()) -> concurrent.unit
-            case State(true, queue) => State[F](locked = true, queue.enqueue(signal)) -> poll(signal.get).onCancel(cleanup)
-          }.flatten
-        }
-      }
-
-      override def release = state.modify {
-        case State(false, _) => unlocked[F] -> concurrent.unit
-        case State(true, queue) =>
-          if (queue.isEmpty) unlocked[F] -> concurrent.unit
-          else {
-            val (signal, rest) = queue.dequeue
-            State[F](locked = true, rest) -> signal.complete(()).void
-          }
-      }.flatten
-    }
-
-}
-
 abstract class Mutex {
   def acquire: IO[Unit]
   def release: IO[Unit]
@@ -135,6 +88,54 @@ object Mutex {
         }
     }.flatten
   }
+}
+
+
+// generic mutex after the polymorphic concurrent exercise
+abstract class MutexV2[F[_]] {
+  def acquire: F[Unit]
+  def release: F[Unit]
+}
+
+object MutexV2 {
+  type Signal[F[_]] = Deferred[F, Unit]
+  case class State[F[_]](locked: Boolean, waiting: Queue[Signal[F]])
+
+  def unlocked[F[_]] = State[F](locked = false, Queue())
+  def createSignal[F[_]](using concurrent: Concurrent[F]): F[Signal[F]] = concurrent.deferred[Unit]
+
+  def create[F[_]](using concurrent: Concurrent[F]): F[MutexV2[F]] =
+    concurrent.ref(unlocked).map(initialState => createMutexWithCancellation(initialState))
+
+  def createMutexWithCancellation[F[_]](state: Ref[F, State[F]])(using concurrent: Concurrent[F]): MutexV2[F] =
+    new MutexV2[F] {
+      override def acquire = concurrent.uncancelable { poll =>
+        createSignal.flatMap { signal =>
+
+          val cleanup = state.modify {
+            case State(locked, queue) =>
+              val newQueue = queue.filterNot(_ eq signal)
+              State(locked, newQueue) -> release
+          }.flatten
+
+          state.modify {
+            case State(false, _) => State[F](locked = true, Queue()) -> concurrent.unit
+            case State(true, queue) => State[F](locked = true, queue.enqueue(signal)) -> poll(signal.get).onCancel(cleanup)
+          }.flatten
+        }
+      }
+
+      override def release = state.modify {
+        case State(false, _) => unlocked[F] -> concurrent.unit
+        case State(true, queue) =>
+          if (queue.isEmpty) unlocked[F] -> concurrent.unit
+          else {
+            val (signal, rest) = queue.dequeue
+            State[F](locked = true, rest) -> signal.complete(()).void
+          }
+      }.flatten
+    }
+
 }
 
 object MutexPlayground extends IOApp.Simple {
